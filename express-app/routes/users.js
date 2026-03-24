4208b92d06c9cc12c04e962a160b1eb69c8591fe
Andrew Goodman <goodsoul.andrew@mail.ru>
2026-03-24 18:43:46 +0500

    task 5

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,20 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const users = [
   {
-    "id": 1,
-    "name": "Goodsoul Andrew"
+    id: 1,
+    name: "Goodsoul Andrew",
   },
   {
-    "id": 2,
-    "name": "Volkov Vladislav"
-  }
-]
+    id: 2,
+    name: "Volkov Vladislav",
+  },
+];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   res.send(users);
+});
+
+router.post("/", function (req, res, next) {
+  const maxId = users[users.length - 1].id;
+  // console.log(req.body)
+  const newUser = {
+    id: maxId + 1,
+    name: req.body.name,
+  };
+  users.push(newUser);
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
